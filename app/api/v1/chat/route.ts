d303fdc3089bc2bfabd3173832a51804a2a32d7a
Ali Farooq <alidotm.me@gmail.com>
2025-06-10 04:17:28 +0500

    refactor: Updated web search tool name

--- app/api/v1/chat/route.ts
+++ app/api/v1/chat/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: Request) {
 
     if (search && mdlConfig.supportsWebSearch) {
       tools = {
-        webSearchTool: webSearchTool(),
+        webSearch: webSearchTool(),
       };
     }
 
